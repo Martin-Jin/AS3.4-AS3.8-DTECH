5ccaa9a4173f5673287babe9f6c7c02c0f99078b
averageVariable <109107515+averageVariable@users.noreply.github.com>
2024-05-14 22:31:47 +0000

    V1.9 - Added in working slide show.

--- htmlJs/htmlJs_index.js
+++ htmlJs/htmlJs_index.js
@@ -16,10 +16,41 @@ const index_MEDIAQUERIES = [{
   }
 }];
 
+//Array of images in the slide
+const index_SLIDEIMAGES = [{ image: '/images/images_slide/slide_coffee.jpg', alt: 'coffee1' },
+                           { image: '/images/images_slide/slide_coffee.jpg', alt: 'coffee2' },
+                           { image: '/images/images_slide/slide_coffee.jpg', alt: 'coffee3' }]
+
+let index_slideIndex = -1;
+const index_SLIDE_IMAGE_ID = 'index_slideImage';
 /**************************************************************/
 // START OF MODULE
 /**************************************************************/
 htmlJs_setUpMediaQuery(index_MEDIAQUERIES);
+
+/**************************************************************/
+// function index_displaySlide();
+// Determines what slide to display next when the user clicks
+// on the foward or back button
+// called when user clicks on foward or back arrow on the slide show
+// input: +1 or -1 to increase or decrease the slide show index
+/**************************************************************/
+function index_displaySlide(index) {
+  console.log("index_displaySlide(" + index + ");");
+  //Determining what slide to display next
+  index_slideIndex += index;
+  //When slide goes past 0 or the max length of the amount of images,
+  //change the index accordingly
+  if (index_slideIndex < 0) { index_slideIndex = index_SLIDEIMAGES.length - 1; }
+  else if (index_slideIndex > index_SLIDEIMAGES.length - 1) { index_slideIndex = 0; };
+  console.log("SlideIndex: " + index_slideIndex);
+  //Displaying the next slide
+  let old_img = document.getElementById(index_SLIDE_IMAGE_ID);
+  let img = index_SLIDEIMAGES[index_slideIndex].image;
+  let alt = index_SLIDEIMAGES[index_slideIndex].alt;
+  old_img.src = img;
+  old_img.alt = alt;
+}
 /**************************************************************/
 // END OF MODULE
 /**************************************************************/
