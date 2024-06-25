1d6226ca9c64442872c6f1cf0c32079ac9c5e735
Martin-Jin <109107515+Martin-Jin@users.noreply.github.com>
2024-06-25 23:08:35 +0000

    V3.6 - Added images for coffee, and added the new images to the slide show.

--- htmlJs/htmlJs_general.js
+++ htmlJs/htmlJs_general.js
@@ -7,152 +7,23 @@ console.log('%c' + MODULENAME + ': ', 'color: red;');
 /**************************************************************/
 // Variables and constants
 /**************************************************************/
-//Contains the pages to be generated on the navBar
-// const general_PAGES_HOME = 
-// { url: '/../index.html', name: 'Home', icon: '/images/images_icons/icons_coffee.png', 
-//  alt: 'coffee_icon', id: 'home', styling: '' };
-// const general_PAGES_ABOUT_US =
-// { url: '/../index.html', name: 'About us', icon: '/images/images_icons/icons_about.png', 
-//  alt: 'info_icon', id: 'aboutUs', styling: ''  };
-// const general_PAGES_CONTACT_US =
-// { url: '/html/html_contact', name: 'Contact us', icon: '/images/images_icons/icons_contact.png', 
-//  alt: 'phone_icon', id: 'contactUs', styling: ''  };
-// const general_PAGES_PRODUCTS =
-// { url: '/html/html_products', name: 'Products', icon: '/images/images_icons/icons_products.png', 
-//  alt:'coffee_icon', id: 'products', styling: ''  };
-// const general_PAGES_ORDERS =
-// { url: '/html/html_orders', name: 'Orders', icon: '/images/images_icons/icons_orders.png', 
-//  alt: 'basket_icon', id: 'orders', styling: ''  };
-// const general_PAGES = [general_PAGES_HOME, general_PAGES_ABOUT_US, general_PAGES_CONTACT_US, general_PAGES_PRODUCTS, general_PAGES_ORDERS];
 
-// //The media query break point where the device is considered no longer a desktop
-// const general_MEDIA_QUERY_BREAK_POINT = 1350;
-// //Mobile by deafult, set to true when break point triggered
-// let general_mobile = true;
 
-// //The html navbar element
-// const general_NAVBAR = document.getElementById("navBar");
+/**************************************************************/
+// START OF MODULE
+/**************************************************************/
 
-// /**************************************************************/
-// // START OF MODULE
-// /**************************************************************/
+/**************************************************************/
+// function index_displayCard();
+// sets the info
+// on the foward or back button
+// called when user clicks on foward or back arrow on the slide show
+// input: +1 or -1 to increase or decrease the slide show index
+/**************************************************************/
+function index_displaySlide(index) {
+  console.log("index_displaySlide(" + index + ");");
+}
 
-// //Collapsing nav on load for mobile
-// general_collapseNav();
-
-// /**************************************************************/
-// // function general_generateNav(navElements, navBar, styling);
-// // generates a navbar by appending buttons to a given id
-// // Input: array of objects that have the url and name of the page, and the navBar element
-// // also optional styling parameter to add any extra classes
-// // Called by multiple functions to generate navBar buttons for navBar and also drop down menu
-// /**************************************************************/
-// function general_generateNav(navElements, navBar, styling) {
-//   //console.log("general_generateNav();");
-//   //Iterating through all an array of objects which contain the name of the page and the url
-//   //This is then displayed on the navBar
-//   for (i = 0; i < navElements.length; i++) {
-//     let page = navElements[i];
-//     //Giving the button any general classes given, as well as any classes just for the button
-//     let classes = styling + page.styling;
-    
-//     //If given an icon then put it in the button
-//     if (page.icon != '') {img = `<img src='${page.icon}' 
-//     alt='${page.alt}' class='general_icon'>`} else {img = ``;}
-    
-//     //Creating the button to go on the navbar
-//     let button =
-//       `<button onclick="(function() { window.location = '${page.url}' })()" 
-//     class='${classes}' id='${page.id}'>
-//     ${page.name} ${img}
-//     </button>`;
-//     navBar.innerHTML += button;
-//   }
-// }
-
-// /**************************************************************/
-// // function general_clearNav(navBar);
-// // clears a given navBar
-// /**************************************************************/
-// function general_clearNav(navBar) {
-//   //console.log("general_clearNav();");
-//   navBar.innerHTML = "";
-// }
-
-// /**************************************************************/
-// // function general_mediaQuery(mediaQueryListObject, callBack);
-// // Checks if user is on mobile or desktop when media query is triggered
-// // Input: the media query to check, and the call back to call when it is
-// // Called by general_setUpMediaQuery();
-// /**************************************************************/
-// function general_mediaQuery(mediaQueryListObject, callBack) {
-//   //console.log("general_mediaQuery();");
-//   //If mediaQuery matches then call the call back and set mobile to false as user on desktop
-//   if (mediaQueryListObject.matches) {
-//     callBack();
-//     general_mobile = false;
-//     //Other wise collapse navBar since user is on mobile
-//   } else { general_mobile = true; general_collapseNav(); }
-// }
-
-// /**************************************************************/
-// // function general_collapseNav();
-// // Collapses navBar into a three bar menu and home page
-// // Called as a callback by general_mediaQuery when the screen becomes small
-// /**************************************************************/
-// function general_collapseNav() {
-//   //If user on desktop don't collapse navBar
-//   if (general_mobile === false) { return; }
-//   //console.log("general_collapseNav();");
-//   //Regenerating the navbar so that there is only the home button
-//   general_clearNav(general_NAVBAR);
-//   general_generateNav([general_PAGES[0]], general_NAVBAR, '');
-//   //Adding drop down button to navBar
-//   general_NAVBAR.innerHTML +=
-//     `<div class="general_dropDown">
-//     <button onclick="(function() { document.getElementById('dropDown').classList.toggle('show'); })()" 
-//       id="general_dropBtn" class="aleo-general">
-//         Dropdown
-//       </button>
-//       <div id="dropDown" class="general_dropDown_content">
-//       </div>
-//     </div>`
-//   //Generating all the buttons inside the drop down menu other than the home page
-//   general_generateNav(general_PAGES.slice(1, general_PAGES.length + 1), document.getElementById("dropDown"), '');
-//   //Close the dropdown menu if the user clicks outside of it
-//   window.onclick = function(event) {
-//     if (!event.target.matches('#general_dropBtn')) {
-//       let dropDowns = document.getElementsByClassName("general_dropDown_content");
-//       for (i = 0; i < dropDowns.length; i++) {
-//         let openDropdown = dropDowns[i];
-//         if (openDropdown.classList.contains('show')) {
-//           openDropdown.classList.remove('show');
-//         }
-//       }
-//     }
-//   }
-// }
-
-// /**************************************************************/
-// // function general_setUpMediaQuery();
-// // Sets up all the necessary media queries necessary for a page
-// // Called on load at each page
-// /**************************************************************/
-// function general_setUpMediaQuery(mediaQueries) {
-//   //console.log("general_setUpMediaQuery();");
-//   for (i = 0; i < mediaQueries.length; i++) {
-//     let mediaQuery = mediaQueries[i].mediaQuery;
-//     let callBack = mediaQueries[i].callBack;
-//     mediaQuery.addEventListener("change", () => {
-//       general_mediaQuery(mediaQuery, callBack);
-//     });
-//     //Calling the function right away so if the user loads into the page on a desktop
-//     //it will trigger the media query
-//     //Other wise media query won't be triggered until the user changes tab size
-//     //Since the listener listens for the media query to be triggered
-//     general_mediaQuery(mediaQuery, callBack);
-//   }
-// }
-// /**************************************************************/
-// //    END OF MODULE
-// /**************************************************************/
+/**************************************************************/
+//    END OF MODULE
+/**************************************************************/
--- htmlJs/htmlJs_index.js
+++ htmlJs/htmlJs_index.js
@@ -7,30 +7,21 @@ console.log('%c' + MODULENAME + ': ', 'color: red;');
 /**************************************************************/
 // Variables and constants
 /**************************************************************/
-// //Set active page to index.html
-// general_PAGES_HOME.styling = "active";
+//Array of images in the slide
+const coffee1 = { image: '/images/images_coffee/coffee_1.jpg', alt: 'coffee1', price: 3.50, name: 'latte', about: 'A classic Italian coffee drink. This drink is known for its smooth and velvety texture and its balance of espresso and steamed milk, creating a perfect harmony of flavors.', ingredients: 'Espresso and steamed milk.' };
+const coffee2 = { image: '/images/images_coffee/coffee_2.jpg', alt: 'coffee2', price: 4.25, name: 'cappuccino', about: 'A popular coffee drink with a rich, creamy texture.  The cappuccino features a distinct layer of foamed milk, giving it a beautiful presentation and a light, airy texture.  It is a delightful choice for those who prefer a slightly milder coffee experience with a touch of sweetness.', ingredients: 'Espresso, steamed milk, and foamed milk.' };
+const coffee3 = { image: '/images/images_coffee/coffee_3.jpg', alt: 'coffee3', price: 5.00, name: 'espresso', about: 'A concentrated coffee drink with a bold flavor.  The espresso is a true coffee connoisseur’s choice, delivering a strong and intense coffee experience in a small serving.  It is known for its rich aroma and its ability to awaken the senses with its bold and intense flavor.', ingredients: 'Espresso.' };
+const coffee4 = { image: '/images/images_coffee/coffee_4.jpg', alt: 'coffee4', price: 3.75, name: 'americano', about: 'A coffee drink that is similar to black coffee but with a slightly stronger flavor.  The Americano is a perfect choice for those who enjoy the simplicity of black coffee but crave a slightly more intense flavor.  It is a refreshing and invigorating drink that balances the bold espresso with the clean taste of hot water.', ingredients: 'Espresso and hot water.' };
+const coffee5 = { image: '/images/images_coffee/coffee_5.jpg', alt: 'coffee5', price: 4.50, name: 'mocha', about: 'A coffee drink with a rich chocolate flavor.  The mocha is a decadent and indulgent treat that combines the richness of espresso with the sweetness of chocolate syrup and the creamy texture of steamed milk.  It is a perfect choice for those who crave a sweet and satisfying coffee experience.', ingredients: 'Espresso, chocolate syrup, steamed milk, and whipped cream.' };
+const coffee6 = { image: '/images/images_coffee/coffee_6.jpg', alt: 'coffee6', price: 5.25, name: 'frappuccino', about: 'A refreshing blended iced coffee drink.  The Frappuccino is a perfect choice for those who enjoy a cool and refreshing treat.  It combines the bold flavor of espresso with the sweetness of milk, ice, and flavorings, creating a delightful and satisfying experience.', ingredients: 'Espresso, milk, ice, and flavorings.' };
 
-// const index_MEDIAQUERIES = [{
-//   mediaQuery: window.matchMedia("(min-width: " + general_MEDIA_QUERY_BREAK_POINT + "px)"), callBack: () => {
-//     //Clearing the navbar then generating the navbar and the sign in button
-//     // general_clearNav(general_NAVBAR);
-//     // general_generateNav(general_PAGES, general_NAVBAR, '');
-//     // general_generateNav([{ url: '/html/html_signIn', name: 'Sign in', icon: '', 
-//     //                       alt: '', id: 'signIn', styling: 'general_signInBtn' }], general_NAVBAR, '');
-//   }
-// }];
+const index_SLIDEIMAGES = [coffee1, coffee2, coffee3, coffee4, coffee5, coffee6];
 
-// //Array of images in the slide
-// const index_SLIDEIMAGES = [{ image: '/images/images_slide/slide_coffee.jpg', alt: 'coffee1' },
-//                            { image: '/images/images_slide/slide_coffee.jpg', alt: 'coffee2' },
-//                            { image: '/images/images_slide/slide_coffee.jpg', alt: 'coffee3' }]
-
-// let index_slideIndex = -1;
-// const index_SLIDE_IMAGE_ID = 'index_slideImage';
-// /**************************************************************/
-// // START OF MODULE
-// /**************************************************************/
-// general_setUpMediaQuery(index_MEDIAQUERIES);
+let index_slideIndex = -1;
+const index_SLIDE_IMAGE_ID = 'index_slideImage';
+/**************************************************************/
+// START OF MODULE
+/**************************************************************/
 
 /**************************************************************/
 // function index_displaySlide();
@@ -49,11 +40,11 @@ function index_displaySlide(index) {
   else if (index_slideIndex > index_SLIDEIMAGES.length - 1) { index_slideIndex = 0; };
   console.log("SlideIndex: " + index_slideIndex);
   //Displaying the next slide
-  let old_img = document.getElementById(index_SLIDE_IMAGE_ID);
+  let prev_img = document.getElementById(index_SLIDE_IMAGE_ID);
   let img = index_SLIDEIMAGES[index_slideIndex].image;
   let alt = index_SLIDEIMAGES[index_slideIndex].alt;
-  old_img.src = img;
-  old_img.alt = alt;
+  prev_img.src = img;
+  prev_img.alt = alt;
 }
 /**************************************************************/
 // END OF MODULE
